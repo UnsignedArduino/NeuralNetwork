f8f8ad29164d91843a18967ed098dc33bb90eeb2
UnsignedArduino <UnsignedArduino@outlook.com>
2021-09-30 22:21:04 -0400

    Why doesn't this work AHHHHHHHHHH

---
+++ main.py
@@ -0,0 +1,59 @@
+from random import randint
+from neuralnetwork import NeuralNetwork
+
+
+topology = [2, 2, 2, 1]
+
+networks = []
+fitnesses = []
+
+for _ in range(200):
+    networks.append(NeuralNetwork(topology))
+    fitnesses.append(0)
+
+outputs = []
+best_ever = NeuralNetwork(topology)
+best_ever_fitness = 0
+
+all_inserts = [[0, 1], [0, 0], [1, 0], [1, 1]]
+
+for round in range(2000):
+    inputs = [randint(0, 1), randint(0, 1)]
+    for network_index, network in enumerate(networks):
+        for insert_index, insert in enumerate(all_inserts):
+            inputs = [all_inserts[insert_index][0], all_inserts[insert_index][1]]
+            output = network.feed_forward(inputs)
+            expected = 0
+            if inputs[0] == inputs[1] and output[0] < 0.5:
+                fitnesses[network_index] += 1 - output[0]
+            elif output[0] > 0.5 and inputs[0] != inputs[1]:
+                fitnesses[network_index] += output[0] - 0.5
+            else:
+                fitnesses[network_index] -= 1
+            error = abs(expected - output[0])
+    best = 0
+    highest = 0
+    elitists = [0, 0]
+    for network_index, network in enumerate(networks):
+        if highest < fitnesses[network_index]:
+            elitists[0] = elitists[1]
+            elitists[1] = network_index
+            highest = fitnesses[network_index]
+            best = network_index
+        fitnesses[network_index] = 0
+    best_network = networks[elitists[0]]
+    if round % 10 == 0:
+        print(f"Round {round} highest fitness: {highest}")
+    if highest > best_ever_fitness:
+        best_ever_fitness = highest
+        best_ever = best_network
+        print(f"New highest fitness: {highest}")
+    for network_index, network in enumerate(networks):
+        networks[network_index] = networks[network_index].crossover(networks[elitists[0]], networks[elitists[1]])
+        networks[network_index].mutate(0.2)
+
+for _ in range(30):
+    test_input = [randint(0, 1), randint(0, 1)]
+    output = best_ever.feed_forward(test_input)
+    print(f"Test case: {test_input}")
+    print(f"Output: {output}")
--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 from math import e
+from copy import deepcopy
 from random import random, uniform as randfloat
 
 
@@ -48,6 +51,11 @@ class Layer:
     A layer of neurons.
     """
     def __init__(self, neuron_counts: int):
+        """
+        Make the layer.
+
+        :param neuron_counts: How many neurons are in this layer.
+        """
         self.neurons = []
         for _ in range(neuron_counts):
             self.neurons.append(Neuron())
@@ -64,13 +72,21 @@ class Connection:
     """
     A connection from a neuron in a layer to another neuron in another layer.
     """
-    def __init__(self, from_layer: Layer, from_neuron: Neuron,
-                 to_layer: Layer, to_neuron: Neuron):
+    def __init__(self, from_layer: int, from_neuron: int,
+                 to_layer: int, to_neuron: int, weight: float):
+        """
+        Make a connection between neurons.
+
+        :param from_layer: The layer the original neuron is on.
+        :param from_neuron: The original neuron.
+        :param to_layer: The layer the neuron to connect to is on.
+        :param to_neuron: The neuron to connect to.
+        """
         self.from_layer = from_layer
         self.from_neuron = from_neuron
         self.to_layer = to_layer
         self.to_neuron = to_neuron
-        self.weight = 0
+        self.weight = weight
         self.min_max = 1
 
     def mutateWeight(self):
@@ -85,3 +101,121 @@ class Connection:
             self.weight = self.min_max
         if self.weight < -self.min_max:
             self.weight = -self.min_max
+
+
+class NeuralNetwork:
+    """
+    A entire neural network composed of layers composed of neurons connected
+    to each other.
+    """
+    def __init__(self, topology: list[int]):
+        """
+        Make a neural network.
+
+        :param topology: A topology of the network, like [2, 3, 1] for
+         3 layers with layer 1 having 2 neurons, layer 2 having 3 neurons,
+         and layer 3 having 1 neuron.
+        """
+        self.layers = []
+        self.bias = 0
+        for neuron_count in topology:
+            self.layers.append(Layer(neuron_count))
+        self.layers[0].neurons.append(Neuron())
+        self.layers[0].neurons[0].sum = 1
+        self.fully_connect()
+
+    def fully_connect(self):
+        """
+        Connect all the neurons to each other and initialize them all with
+        random weights.
+        """
+        for layer_index, layer in enumerate(self.layers):
+            for neuron_index, neuron in enumerate(layer.neurons):
+                try:
+                    for next_neuron_index, next_neuron in enumerate(self.layers[layer_index + 1].neurons):
+                        self.layers[layer_index].neurons[neuron_index].connections.append(
+                            Connection(layer_index, neuron_index, layer_index + 1, next_neuron_index, randfloat(-1, 1))
+                        )
+                except IndexError:
+                    pass
+        self.layers[0].neurons[-1].connections.clear()
+        self.layers[0].neurons[-1].sum = 1
+        for layer_index, layer in enumerate(self.layers):
+            for neuron_index, neuron in enumerate(layer.neurons):
+                self.layers[0].neurons[-1].connections.append(
+                    Connection(layer_index, neuron_index, 0, self.bias, randfloat(-1, 1))
+                )
+
+    def reset_neurons(self):
+        """
+        Reset all the neurons.
+        """
+        for layer_index, layer in enumerate(self.layers):
+            for neuron_index, neuron in enumerate(layer.neurons):
+                self.layers[layer_index].neurons[neuron_index].sum = 0
+        self.layers[0].neurons[-1].sum = 1
+
+    def sigmoid_layer(self, layer: int):
+        """
+        Sigmoid-ify layer.
+
+        :param layer: The layer to run the sigmoid function on.
+        """
+        self.layers[layer].sigmoid()
+
+    def step_layer(self, layer: int):
+        """
+        Step a layer forward.
+
+        :param layer: The layer to step.
+        """
+        for neuron_index, neuron in enumerate(self.layers[layer].neurons):
+            conn1 = self.layers[layer].neurons[neuron_index].connections
+            for conn_index, conn in enumerate(conn1):
+                layer2 = conn1[conn_index].to_layer
+                self.layers[layer2].neurons[conn1[conn_index].to_neuron].sum += self.layers[layer].neurons[neuron_index].sum * conn1[conn_index].weight
+
+    def feed_forward(self, insert_target: list[float]) -> list[float]:
+        """
+        Feed forward the entire neural network.
+
+        :param insert_target: What to insert.
+        :return: A list of floats.
+        """
+        for insert_index, insert in enumerate(insert_target):
+            self.layers[0].neurons[insert_index].sum = insert
+        for layer_index, layer in enumerate(self.layers):
+            self.step_layer(layer_index)
+            self.sigmoid_layer(layer_index)
+        output = []
+        for neuron_index, neuron in enumerate(self.layers[-1].neurons):
+            output.append(self.layers[-1].neurons[neuron_index].sum)
+        self.reset_neurons()
+        return output
+
+    def mutate(self, rate: float):
+        """
+        Mutate the entire network.
+
+        :param rate: Mutation rate as a float.
+        """
+        for layer_index, layer in enumerate(self.layers):
+            for neuron_index, neuron in enumerate(layer.neurons):
+                self.layers[layer_index].neurons[neuron_index].mutate(rate)
+
+    def crossover(self, parent1: NeuralNetwork, parent2: NeuralNetwork) -> NeuralNetwork:
+        """
+        Crossover self with other parent.
+
+        :param parent1: A parent.
+        :param parent2: The other parent.
+        :return: Another neural network.
+        """
+        child = deepcopy(parent1)
+        for layer_index, layer in enumerate(self.layers):
+            for neuron_index, neuron in enumerate(layer.neurons):
+                conns = parent2.layers[layer_index].neurons[neuron_index].connections
+                for p2_n_idx, p2_n in enumerate(conns):
+                    if randchance(0.5):
+                        child.layers[layer_index].neurons[neuron_index].connections[p2_n_idx] = conns[p2_n_idx]
+        return child
